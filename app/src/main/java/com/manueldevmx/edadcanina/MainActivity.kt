8e8981545104b9454616b4c229ecf194e5369ab5
manueldevmx <43160136+manueldevmx@users.noreply.github.com>
2022-04-27 01:35:54 -0500

    update

--- app/src/main/java/com/manueldevmx/edadcanina/MainActivity.kt
+++ app/src/main/java/com/manueldevmx/edadcanina/MainActivity.kt
@@ -2,10 +2,24 @@ package com.manueldevmx.edadcanina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ageEdit = findViewById<EditText>(R.id.age_edit)
+        val resultText = findViewById<TextView>(R.id.result_text)
+        val button = findViewById<Button>(R.id.btn_calcular)
+
+        button.setOnClickListener {
+            val ageString = ageEdit.text.toString()
+            val ageInt = ageString.toInt()
+            val result = ageInt * 7
+            resultText.text = "Your dog age is: $result years old."
+        }
     }
 }
